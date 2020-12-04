3976089d5ec2d4b962cbcdf1a9787ac4e60258e4
ghddbsrl <dbsrl8777@naver.com>
2020-12-05 01:48:04 +0900

    add embed

--- Dice.py
+++ Dice.py
@@ -1,21 +1,24 @@
-from discord.ext import commands
+import discord
+import asyncio
 import random
-
+from discord import Member
+from discord.ext import commands
 TOKEN = ""
 
-client = commands.Bot(command_prefix='/')
 
-@client.command(name='주사위')
-async def roll(ctx, number):
-    await ctx.send("주사위 굴리는 중")
-    await ctx.send("...")
-    await ctx.send("...")
-    await ctx.send("...")
-    await ctx.send("...")
-    await ctx.send(f'주사위를 굴려 {random.randint(1,int(number))}이(가) 나왔습니다. (1-{number})')
+client = discord.Client()
+@client.event
+async def on_message(message):
 
-@roll.error
-async def roll_error(ctx, error):
-    await ctx.send("명령어 오류. \" /주사위 숫자(1이상) \"")
-    
+    if message.content.startswith("/주사위"):
+        Text = ""
+        Text = Text + str(random.randint(1,6))
+
+        print(Text.strip())
+        embed = discord.Embed(
+            title="주사위!",
+            description=Text.strip(),
+            colour=discord.Color.red()
+        )
+        await message.channel.send(message.channel, embed=embed)        
 client.run(TOKEN)
