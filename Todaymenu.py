09dfe3b7e4cc505d5b451fc22a6968a2684d0031
ghddbsrl <dbsrl8777@naver.com>
2020-12-06 01:37:17 +0900

    add embed

--- Todaymenu.py
+++ Todaymenu.py
@@ -1,15 +1,26 @@
-from discord.ext import commands
+import discord
+import asyncio
 import random
-
+from discord import Member
+from discord.ext import commands
 TOKEN = ""
 
-client = commands.Bot(command_prefix='/')
 
-@client.command(name='메뉴')
-async def menu(ctx):
-    menulist = ["한식","중식","일식","양식","치킨","떡볶이"]
-    i = random.randint(0,6)
+client = discord.Client()
+@client.event
+async def on_message(message):
 
-    await ctx.send(menulist[i])
-    
+    if message.content.startswith("/메뉴"):
+        Text = ""
+        arr = ["한식", "중식", "양식", "일식", "치킨"]
+        i = random.randint(1,5)
+        Text = arr[i]
+
+        print(Text.strip())
+        embed = discord.Embed(
+            title="메뉴!",
+            description=Text.strip(),
+            colour=discord.Color.red()
+        )
+        await message.channel.send(message.channel, embed=embed)        
 client.run(TOKEN)
