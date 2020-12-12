d0067417275fef30197d136a9c6bbc37398fb381
ghddbsrl <dbsrl8777@naver.com>
2020-12-12 18:05:54 +0900

    updated all command

--- AllCommand.py
+++ AllCommand.py
@@ -17,7 +17,15 @@ async def on_message(message):
             colour = discord.Colour.blue()
         )
         
-        embed.add_field(name = '/명령', value = '상황',inline = False)
+        embed.add_field(name = '/안녕', value = '봇과 인사',inline = False)
+        embed.add_field(name = '/주사위', value = '6면체 주사위 굴리기',inline = False)
+        embed.add_field(name = '/이모티콘', value = '추천 이모티콘',inline = False)
+        embed.add_field(name = '/복권', value = '복권번호 7개 추천',inline = False)
+        embed.add_field(name = '/한판더', value = '게임 한판더 할까',inline = False)
+        embed.add_field(name = '/시간', value = '현재시간',inline = False)
+        embed.add_field(name = '/타이머 n', value = 'n만큼 타이머',inline = False)
+        embed.add_field(name = '/메뉴', value = '메뉴추천',inline = False)
+        embed.add_field(name = '/게임을 시작하자!', value = '봇과 가위바위보',inline = False)
         
         await message.channel.send(channel,embed=embed)       
 client.run(TOKEN)
